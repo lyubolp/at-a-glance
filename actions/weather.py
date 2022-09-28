9bf961eb0b501dfec2f19bcb6c7339f83e9bbae8
lyubolp <lyubo.karev@gmail.com>
2022-09-28 10:22:27 +0300

    Improved weather action

--- actions/weather.py
+++ actions/weather.py
@@ -3,6 +3,7 @@ import sys
 import json
 
 from action import Action
+from datetime import datetime
 
 
 class WeatherAction(Action):
@@ -15,16 +16,48 @@ class WeatherAction(Action):
         self.__city_name = city_name
 
     def call(self) -> str:
-        url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric'
-        url = url.format(lat=self.__lat, lon=self.__lon, api_key=self.__api_key)
+        weather_now = self.__get_weather_now()
+        weather_today = self.__get_weather_today()
+        return weather_now + '\n' + weather_today
 
-        resp = requests.get(url=url)
+    def __get_weather_now(self) -> str:
+        weather_now = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric'
+        response = self.__get_weather_from_api(weather_now)
+
+        weather_like = response['weather'][0]['main']
+        temperature = response['main']['temp']
+        result = 'The weather in ' + self.__city_name + ' is ' + weather_like + \
+                 '. The temperature now is ' + str(int(temperature)) + '°'
+
+        return result
+
+    def __get_weather_today(self) -> str:
+        weather_today = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}&units=metric'
+
+        response = self.__get_weather_from_api(weather_today)
+
+        weather = response['list']
+        current_day = datetime.now().strftime('%Y-%m-%d')
+        temp_today = [WeatherAction.__get_weather_text_for_today(item) for item in weather if current_day in item['dt_txt']]
+
+        result = 'The weather in ' + self.__city_name + ' today will be ' + '. '.join(temp_today)
+
+        return result
+
+    @staticmethod
+    def __get_weather_text_for_today(weather_hour: dict) -> str:
+        weather_temp = str(int(weather_hour['main']['temp']))
+        weather_type = weather_hour['weather'][0]['main']
+        weather_hour = weather_hour['dt_txt'].split(' ')[1]
+        return weather_temp + '°, ' + weather_type + ', at ' + weather_hour
+
+    def __get_weather_from_api(self, request_url: str):
+        request_url = request_url.format(lat=self.__lat, lon=self.__lon, api_key=self.__api_key)
+
+        # print(request_url)
+
+        resp = requests.get(url=request_url)
         if resp.status_code != 200:
             return 'Invalid api key or city name'
 
-        weather_like = resp.json()['weather'][0]['main']
-        temperature = resp.json()['main']['temp']
-        result = 'The weather in ' + self.__city_name + ' is ' + weather_like + \
-                 '. The temperature is ' + str(temperature) + ' degrees celsius'
-
-        return result
+        return resp.json()
