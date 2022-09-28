f05aa598807eea7fea19712a0910dac132dc7883
lyubolp <lyubo.karev@gmail.com>
2022-09-28 10:27:20 +0300

    Added Greeting action to the main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from actions.greeting import GreetingAction
 from actions.weather import WeatherAction
 from config import Config
 
@@ -5,12 +6,13 @@ if __name__ == '__main__':
     config = Config()
     config.load('config.json')
 
+    greet = GreetingAction(config['personal']['name'])
     weather = WeatherAction(config['weather']['location'][0],
                             config['weather']['location'][1],
                             config['weather']['api_key'],
                             config['weather']['location'][2])
 
-    actions = [weather]
+    actions = [greet, weather]
 
     for action in actions:
         print(action.call())
